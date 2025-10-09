4e58d8ebb6f50bebee5e18fb5595f444895b5b75
david <davidorias0709@gmail.com>
2025-10-09 08:11:08 -0600

    Cambios en ball.h

--- src/cpp/headers/Ball.h
+++ src/cpp/headers/Ball.h
@@ -10,7 +10,15 @@ class Ball {
     Texture2D texture;
     Vector2 position;
     public:
+        /**
+         * @brief Inicializa el jugador, carga su textura, y
+         * posicion inicial. Esta debe ser llamada despues de
+         * inicializar la pantalla de otra manera habra un error.
+         */
         void init();
+        /**
+         * @brief Renderiza el jugador en pantalla
+         */
         void draw();
         void move();
         void reset();
