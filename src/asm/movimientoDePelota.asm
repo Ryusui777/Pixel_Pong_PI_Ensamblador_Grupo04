a7809f02282f7d7e2a73e3c19ba81756516cfef3
david <davidorias0709@gmail.com>
2025-10-12 07:24:09 -0600

    se cambio el movimiento de la pelota en ensambla

--- src/asm/movimientoDePelota.asm
+++ src/asm/movimientoDePelota.asm
@@ -1,27 +1,34 @@
+
+section .bss
+    velocity_ptr: resq 1
 section .data
     negSign: dd -1.0
-
 section .text
+
 
 global pelotaReverseX
 global pelotaReverseY
+global setVelocityPtr
 
 
-
-
-; rdi -> puntero hacia la coordenada x de velocidad de la pelota
-pelotaReverseX: 
-    vmovss xmm0, dword[rdi]
-    vmovss xmm1, dword[negSign]
-    vmulss xmm2, xmm0, xmm1
-    vmovss dword[rdi], xmm2 
+setVelocityPtr: 
+    mov [velocity_ptr], rdi
     ret
 
-
-; rdi -> puntero hacia la coordenada y de velocidad de la pelota
-pelotaReverseY: 
-    vmovss xmm0, dword[rdi]
+; void
+pelotaReverseX: 
+    mov rax, [velocity_ptr]
+    vmovss xmm0, dword[rax]
     vmovss xmm1, dword[negSign]
     vmulss xmm2, xmm0, xmm1
-    vmovss dword[rdi], xmm2 
+    vmovss dword[rax], xmm2 
+    ret
+
+; void 
+pelotaReverseY: 
+    mov rax, [velocity_ptr]
+    vmovss xmm0, dword[rax+4]
+    vmovss xmm1, dword[negSign]
+    vmulss xmm2, xmm0, xmm1
+    vmovss dword[rax+4], xmm2 
     ret
