df8ea1363721f9d8bb312d7f17e9ff9f8bba4fa0
Enrique Ramirez <enrique.ramirez@ucr.ac.cr>
2025-10-14 21:51:25 -0600

    Mejoras de formato en main

--- src/cpp/headers/EndScreen.h
+++ src/cpp/headers/EndScreen.h
@@ -3,16 +3,13 @@
 #include "shared.h"
 #include "Button.h"
 
+class EndScreen {
+ private:
+  // Paths hacia todos los recursos graficos
+  const char* bg_path = "res/endScreen/bg.png";
+  const char* playAgain_path = "res/endScreen/playAgain.png";
 
-
-class EndScreen{
-    private:
-        // Paths hacia todos los recursos graficos
-        const char* bg_path = "res/endScreen/bg.png";
-        const char* playAgain_path = "res/endScreen/playAgain.png";
-
-        // Members
-        Texture2D blur_bg;
-        Button playAgain;
-
+  // Members
+  Texture2D blur_bg;
+  Button playAgain;
 };
--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -4,57 +4,53 @@
 #include "Menu.h"
 #include "HomeScreen.h"
 
-int main(){
-    // Componentes
-    Window window;
-    Game mainGame;
-    Home homeScreen;
-    Menu menu;
-    
+int main() {
+  // Componentes
+  Window window;
+  Game mainGame;
+  Home homeScreen;
+  Menu menu;
+  
 
-    // Inicializacion de componentes
-    window.initializeWindow();
-    homeScreen.initializeHomeScreen();
-    mainGame.initializeGame();
-    menu.initializeMenu();
+  // Inicializacion de componentes
+  window.initializeWindow();
+  homeScreen.initializeHomeScreen();
+  mainGame.initializeGame();
+  menu.initializeMenu();
 
-    // Ciclo del juego
-    std::uint8_t paused = 0;
-    std::uint8_t inHome = 1;
-    std::uint8_t ended = 0;
-    std::uint8_t inGame = 0;
+  // Ciclo del juego
+  std::uint8_t paused = 0;
+  std::uint8_t inHome = 1;
+  std::uint8_t ended = 0;
+  std::uint8_t inGame = 0;
 
 
-    while(!WindowShouldClose()){
-        window.beginWindowDraw();
-        //? Todo el resto de componentes se renderizan en este periodo
-        if(inHome){
-            homeScreen.drawHomeScreen();
-            homeScreen.hasGameStarted(inGame);
-            inHome = !(inGame);
-            if(inGame) mainGame.resetMatch(); // resetea el juego
-        }
-        else if(inGame){
-            mainGame.setInteractable();
-            mainGame.drawGameElements();
-            mainGame.isGamePaused(paused);
-            inGame = !(paused);
-            if(!inGame) mainGame.setNotInteractable();
-        }
-        else if(paused){
-            mainGame.drawGameElements();
-            menu.drawMenu();
-            menu.gameResumed(inGame);
-            menu.goHome(inHome);
-            paused = !(inGame);
-            if(paused) paused = !(inHome);
-        }
-
-        //?
-        window.endWindowDraw();
+  while (!WindowShouldClose()) {
+    window.beginWindowDraw();
+    //? Todo el resto de componentes se renderizan en este periodo
+    if (inHome) {
+      homeScreen.drawHomeScreen();
+      homeScreen.hasGameStarted(inGame);
+      inHome = !(inGame);
+      if (inGame) mainGame.resetMatch(); // resetea el juego
     }
-
-    window.killWindow(); // Cierra la vantana
-
-    return 0;
+    else if (inGame) {
+      mainGame.setInteractable();
+      mainGame.drawGameElements();
+      mainGame.isGamePaused(paused);
+      inGame = !(paused);
+      if (!inGame) mainGame.setNotInteractable();
+    }
+    else if (paused) {
+      mainGame.drawGameElements();
+      menu.drawMenu();
+      menu.gameResumed(inGame);
+      menu.goHome(inHome);
+      paused = !(inGame);
+      if (paused) paused = !(inHome);
+    }
+    window.endWindowDraw();
+  }
+  window.killWindow(); // Cierra la vantana
+  return 0;
 }
