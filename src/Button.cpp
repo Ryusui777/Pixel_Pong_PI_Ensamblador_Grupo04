98b6d64e75195afb7993933ce02689a0159e0b5e
brandonalfarosaborio <brandonalfarosaborio@gmail.com>
2025-11-24 22:17:20 -0600

    se corrigio la logica del tamano de los botones
    se arreglo merge conflict entre develop y main

--- src/Button.cpp
+++ src/Button.cpp
@@ -15,7 +15,7 @@ void Button::initializeButton(char* imgPath, Vector2 vec) {
 void Button::drawButton() {
   Rectangle src{ 0, 0, (float)texture.width, (float)texture.height };
   Rectangle dst{ position.x, position.y,
-    texture.width * SCALE, texture.height * SCALE };
+  texture.width * 1.0f, texture.height * 1.0f};
   Vector2 origin{ dst.width/2.0f, dst.height/2.0f };
   DrawTexturePro(texture, src, dst, origin, 0.0f, WHITE);
 }
@@ -24,7 +24,7 @@ void Button::drawButton() {
 byte Button::isMouseAboveButton() {
   // float scale = 5.0f;
   Rectangle dst{ position.x, position.y,
-    texture.width * SCALE, texture.height * SCALE };
+  texture.width * 1.0f, texture.height * 1.0f };
   Vector2 origin{ dst.width/2.0f, dst.height/2.0f };
   Rectangle bounds{ dst.x - origin.x, dst.y - origin.y, dst.width, dst.height };
   Vector2 m = GetMousePosition();
